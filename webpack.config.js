3ff21d77ffe9755d1078d953e2252476ef1c6865
Kirubel Girma <kirubelgirma0@gmail.com>
2021-08-04 10:22:16 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  target: "node",
+  devServer: {
+    port: "9500",
+    contentBase: ["./public"],
+    open: true
+  },
+  module: {
+    rules: [{ test: /\.(js|jsx)$/, exclude: /node_modules/, use: { loader: "babel-loader" } }]
+  }
+}
